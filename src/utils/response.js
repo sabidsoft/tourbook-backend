6e25c1959f68bba9081f4dab609cd4930a689800
Sabid Hasan <sabid.soft@gmail.com>
2023-10-30 15:08:48 +0600

    vercel.json file added

--- src/utils/response.js
+++ src/utils/response.js
@@ -2,13 +2,13 @@ exports.errorResponse = (res, { status = 500, message = "Internal server error" 
     res.status(status).json({
         success: false,
         message,
-    })
-}
+    });
+};
 
 exports.successResponse = (res, { status = 200, message = "Success", payload = {} }) => {
     res.status(status).json({
         success: true,
         message,
         data: payload
-    })
-}
+    });
+};
